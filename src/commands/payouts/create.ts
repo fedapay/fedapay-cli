401eb7433f1760f5a2a110eb21d0e25d623fdae0
Saad <saadathmama@gmail.com>
2020-08-07 19:25:55 +0100

    creation d'un payout

--- src/commands/payouts/create.ts
+++ src/commands/payouts/create.ts
@@ -1,26 +1,25 @@
-import {Command, flags} from '@oclif/command'
-import {FedaPay, Payout} from 'fedapay'
+import { Command, flags } from '@oclif/command'
+import { FedaPay, Payout, Transaction } from 'fedapay'
 import * as colorize from 'json-colorizer'
 import Payouts from '../payouts'
-import chalk from 'chalk';
-import inquirer from 'inquirer';
+import { resolve } from 'dns';
 
 export default class PayoutsCreate extends Command {
   static description = 'Create some payouts'
 
   static flags = {
     ...Payouts.flags,
-    data:flags.string({
-      description:'Create a payout',
-      required:false,
+    data: flags.string({
+      description: 'Create a payout',
+      required: false,
 
     }),
-    customer:flags.integer({
-      description:'Create the payout per customer',
-      default:100,
+    customer: flags.integer({
+      description: 'Create the payout per customer',
+      default: 2055,
     }),
-    
-    help:flags.help({char:'h'}),
+
+    help: flags.help({ char: 'h' }),
 
   }
   static examples = [
@@ -33,33 +32,35 @@ export default class PayoutsCreate extends Command {
 
 
   async run() {
-    const {args, flags} = this.parse(PayoutsCreate)
+    const { args, flags } = this.parse(PayoutsCreate)
     const apiKey = flags['api-key']
     const environment = flags.environment
-    const data = flags.data
+     const data = flags.data
     const infos = {
-      "amount" : 2000,
-      "currency" : {"iso" : "XOF"},
+      "amount": 2000,
+      "currency": { "iso": "XOF" },
       "mode": "mtn",
-      "customer" : {
-          "email" : "john.doe@example.com",
-        }
+      "customer": {
+        "email": "john.doe@example.com",
+      }
     }
     //const result = await inquirer.prompt(infos)
 
 
-   FedaPay.setApiKey(apiKey)
-   FedaPay.setEnvironment(environment)
-   try {
-    const payouts = Payout.create({data
-      })
-     this.log(colorize(JSON.stringify(payouts)))
-   } catch (error) {
-     this.log(chalk(`Bad insertion`))
-   }
-   
+    FedaPay.setApiKey(apiKey)
+    FedaPay.setEnvironment(environment)
 
-  
-    
+    try {
+      const payout = await Payout.create(infos)
+      console.log('Succesfully inserted')
+    } catch (error) {
+      console.log(error)
+      // console.log(error ? error.httpResponse : '')
+      // console.log(error && error.httpResponse ? error.httpResponse.data : '')
+      console.error('Bad insertion')
+    }    
+
   }
 }
+
+
