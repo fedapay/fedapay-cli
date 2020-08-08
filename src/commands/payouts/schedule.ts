d2ab923f55d69d8aab2d4a346a904d67c86f1085
Saad <saadathmama@gmail.com>
2020-08-08 13:01:31 +0100

    program a payout

--- src/commands/payouts/schedule.ts
+++ src/commands/payouts/schedule.ts
@@ -1,25 +1,58 @@
 import {Command, flags} from '@oclif/command'
-
+import { FedaPay, Payout, Transaction } from 'fedapay'
+import Payouts from '../payouts'
+import chalk from 'chalk'
+//class for programming a payout 
 export default class PayoutsSchedule extends Command {
-  static description = 'describe the command here'
-
+  static description = 'Program the payout for later'
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    ...Payouts.flags,
+    id: flags.integer({
+      description: 'Program a payout',
+      required: true,
+    }),
+    when: flags.string({
+      description: 'Program with date',
+      default: "01/02/2000 01:02:34",
+      required: true
+    }),
+    data: flags.string({
+      description: 'Program with data',
+      required: true
+    }),
+    help: flags.help({ char: 'h' }),
   }
+  static examples = [
+    'payouts:schedule',
+    'payouts:schedule --id',
+    'payouts:schedule --when',
 
-  static args = [{name: 'file'}]
+  ]
+
+
+
+
+ 
 
   async run() {
-    const {args, flags} = this.parse(PayoutsSchedule)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /home/saadath1/Documents/cli/src/commands/payouts/schedule.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const { flags } = this.parse(PayoutsSchedule)
+    const apiKey = flags['api-key']
+    const environment = flags.environment
+    const id = flags.id
+    const when = flags.when
+    let d = new Date()
+    let a = d.toString()
+    console.log(a)
+    FedaPay.setApiKey(apiKey)
+    FedaPay.setEnvironment(environment)
+    try {
+      //get details of payout
+      const payout = Payout.retrieve(id)
+       ;(await payout).schedule(when)
+      this.log(chalk.green("Succesfully sent")) 
+    } catch (error) {
+      this.error("date not match")
     }
+    
   }
 }
