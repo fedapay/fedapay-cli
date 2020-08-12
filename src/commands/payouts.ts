6746ca0d700b22fc956632fe565b5f371aacda42
Boris Koumondji <brexis2009@yahoo.fr>
2020-08-12 18:15:16 +0100

    Review transactions command

--- src/commands/payouts.ts
+++ src/commands/payouts.ts
@@ -1,33 +1,32 @@
-import {flags} from '@oclif/command'
-import Command from '../base'
+import {flags} from '@oclif/command';
+import Command from '../base';
+
 /**
- * Payouts command class 
+ * Payouts command class
  */
 export default class Payouts extends Command {
-  static description = 'Manage FedaPay payouts ressource'
-  /** 
+  /**
+   * The command descrition
+   * @var string
+   */
+  static description = 'Manage FedaPay payout ressources';
+
+  /**
     * @param string
     * payouts usage string for help
-    
     */
-  static usage = 'payouts <operation> [options]'
+   static usage = 'payouts:<operation> [parameters...]';
+
   /**
-   * @param string[]
-   * examples for the help
+   * The command flags
+   * @var Object
    */
-  static examples = [
-    'payouts list',
-    'payouts create',
-    'payouts update --id  -d  --customer --confirm',
-    'payouts delete  --id --confirm',
-    'payouts schedule --id --when',
-    'payouts send-now',
-  ]
   static flags = {
     ...Command.flags,
-    help: flags.help({char: 'h', description:'Help for payouts\'command'}), 
-  }
+    help: flags.help({ char: 'h', description: 'Help for payouts command.' }),
+  };
+
   async run() {
-    this._help
+    this._help();
   }
 }
