7404a9e3d7918e249295a4354185dc99d5cde4f2
Jean-Philippe-94 <johnanag94@gmail.com>
2020-08-13 13:26:12 +0200

    first pat

--- src/commands/events/list.ts
+++ src/commands/events/list.ts
@@ -1,25 +1,72 @@
-import {Command, flags} from '@oclif/command'
+import { flags } from '@oclif/command';
+import cli from 'cli-ux';
+import { FedaPay, Event } from 'fedapay';
+import colorize from 'json-colorizer';
+import Events from '../events';
+import { boolean } from '@oclif/command/lib/flags';
 
-export default class EventsList extends Command {
-  static description = 'describe the command here'
+/**
+ * EventsList class extending super class Events
+ */
+export default class EventsList extends Events {
+  static description = 'List of the events'
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    ...Events.flags,
+    limit:flags.integer({
+      description:'Limit of the records to display',
+      default:10,
+    }),
+    type:flags.string({
+      description:'Type of the events you want to display',
+    }),
+    date:flags.string({
+      description:'Display events occured a that speficic date',
+    }),
+    methods:flags.string({
+      description:'The method you want to use',
+    }),
+    path:flags.string({
+      description:'Display record for a specific path',
+    }),
+    help: flags.help({char: 'h', description:'Help for events:list command'}),
   }
 
-  static args = [{name: 'file'}]
+  /**
+   * @param Sting[]
+   * Some example of use of the transaction:list command
+   */
+  static examples = [
+    'events:list --api-key=[api_key] --environment=environment --limit=15'
+  ];
+
 
   async run() {
-    const {args, flags} = this.parse(EventsList)
+     /**
+     * @param object
+     * get flags value
+     */
+    const {flags} = this.parse(EventsList)
+     /**
+    * @param String
+    * your api's key
+    */
+   const apiKey = flags['api-key'];
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /home/james/Documents/Programming/cli/src/commands/events/list.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+   /**
+    * @param String
+    * environment or live
+    */
+   const environment = flags.environment;
+
+   /**
+    * @param integer
+    * get the limit value
+    */
+   const limit = flags.limit;
+
+  
+
+
   }
 }
