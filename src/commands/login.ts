8678f410a1b3e900c062a7318dcc0609c37f9406
MAMADOU Saadath <saadathmama@gmail.com>
2020-08-24 10:42:49 +0100

    review login command

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,59 +1,76 @@
-import {flags} from '@oclif/command'
-import Command from '../base'
-import { FedaPay } from 'fedapay'
-import open from 'open'
+import { Command, flags } from '@oclif/command';
+import axios from 'axios';
+import os from 'os';
+import open from 'open';
+
+import fs from 'fs';
 /**
- * import open module
+ * Login Class extending superClass Command
  */
-/**
- * Login class to obtain credentials
- */
+
 export default class Login extends Command {
-  static description = 'Login to Fedapay account'
   /**
    * @param string
-   * static usage of login
+   * Description of the login command
+   */
+  static description = 'Connect to Fedapay account';
+  /**
+   * @param string
+   * login usage
+   */
+
+  static usage = 'login:<operation> [parameters...]';
+  /**
+   * The command flags
+   * @var Object
+   *
    */
 
   static flags = {
     ...Command.flags,
-    help: flags.help({ char: 'h', description: 'Help for the login command' }),
-
-  }
-  /**
-   * require authclient
-   * require to server port
-   * require id of the user
-   * need to get apitoken
-   * authorize url
-   * execute for login flow
-   * retrieve profile id email
-   */
-  
+    help: flags.help({ char: 'h', description: 'Help for payouts command.' }),
+  };
 
   async run() {
-    const { flags } = this.parse(Login)
-    const apiKey = flags['api-key']
-    const environment = flags.environment
-    FedaPay.setApiKey(apiKey)
-    FedaPay.setEnvironment(environment)
+
+    const { flags } = this.parse(Login);
 
     /**
-     * get token and accountId
-     *
+     * required parameters
+     * @params String
      */
-    const token = FedaPay.getToken()
-    /**
-     * open browser
-     */
-    
-    //const browser = await open('https://sindresorhus.com')
+
+    const environment = 'sandbox';
+    const device_name = os.hostname();
     try {
-         console.log(token)
-    } catch (error) {
-          this.error('error')
+      const response = await axios.post('http://localhost:8000/links', {
+        'environment': environment,
+        'device_name': device_name
+      });
+      const data = response.data;
+      const url_browser = data.login_url;
+      open(url_browser);
+      const url = data.poll_url;
+      async function getData() {
+        await axios.get(url).then((res) => {
+          const datas = res.data;
+          datas.forEach(dat => {
+            if ((typeof dat['secret_key'] === 'undefined' || 'null') && (typeof dat['public_key'] === 'undefined' || 'null')) {
+              setTimeout(getData, 5000);
+            }
+            else {
+              fs.writeFile("infos.json", JSON.stringify(data), function (err) {
+                if (err) throw err;
+                console.log('complete');
+              });
+            }
+          });
+        });
+      }
+    getData();
+  }
+    catch (error) {
+      console.log(error);
     }
-
   }
 }
-
