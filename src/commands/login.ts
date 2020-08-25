fa378c75165107614ed2f7418eb351ee4365c26f
MAMADOU Saadath <saadathmama@gmail.com>
2020-08-25 17:30:40 +0100

    login interactive added

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -2,7 +2,8 @@ import { flags, Command } from '@oclif/command'
 import cli from 'cli-ux';
 import axios from 'axios';
 import os from 'os';
-import * as fs from 'fs-extra';
+import * as fs from 'fs';
+import * as path from 'path';
 
 /**
  * Login Class extending superClass Command
@@ -24,9 +25,10 @@ export default class Login extends Command {
       description: 'FedaPay Api environment',
       default: 'sandbox',
     }),
-    interactive: flags.string({
+    interactive: flags.boolean({
       description: 'Login in interactive mode',
       char: 'i',
+      default: true,
     }),
     help: flags.help({ char: 'h', description: 'Help for the login command' }),
   }
@@ -41,7 +43,7 @@ export default class Login extends Command {
   private async sendLinksRequest(device_name: string, environment: string) {
     try {
       // TODO Replace http://brexis-cli.dev.io/links by https://cli.fedapay.com/links
-      const { data } = await axios.post('https://cli.fedapay.com/links', {
+      const { data } = await axios.post('http://localhost:8000/links', {
         device_name, environment
       });
       return data;
@@ -68,10 +70,11 @@ export default class Login extends Command {
   /**
    * Wrap the poll request with a promise. Resolve it when secret key is defined.
    * Reject it when max try is reached
-   * @param poll_url
-   * @return Promise
+   * @param poll_url string
+   *
+   * @return Promise<any>
    */
-  private async checkSecretKey(poll_url: string) {
+  private async checkSecretKey(poll_url: string): Promise<any> {
     return new Promise((resolve, reject) => {
       const maxTries = 10;
       let tries = 0;
@@ -95,6 +98,24 @@ export default class Login extends Command {
   }
 
   /**
+   * Save in config
+   * @param {string} environment Environment
+   * @param {string} secret_key g
+   * @param {string} public_key g
+   */
+  private saveInConfig(environment: string, secret_key: string, public_key: string) {
+    const dir = this.config.configDir;
+    !fs.existsSync(dir) && fs.mkdirSync(dir);
+    const file = path.join(dir, 'config.json');
+
+    fs.writeFile(
+      file,
+      JSON.stringify({ environment, secret_key, public_key }, null, 4),
+      () => { /* */ }
+    );
+  }
+
+  /**
    * The command flags
    * @var Object
    *
@@ -105,50 +126,25 @@ export default class Login extends Command {
       const environment = await cli.prompt('Enter your environment');
       const secret_key = await cli.prompt('Enter your secret_key');
       const public_key = await cli.prompt('Enter your public_key');
-      /**
-       * Parse the key and values into object
-       */
-
-      fs.writeFile('config.json',
-        JSON.stringify({
-          environment,
-          secret_key,
-          public_key
-        }),
-        function (err) {
-          if (err) {
-            console.log(err);
-          }
-        });
-    }
-
-    else {
+      this.saveInConfig(environment, secret_key, public_key);
+    } else {
       const environment = flags.environment;
+
       try {
         const links = await this.sendLinksRequest(os.hostname(), environment);
-
         if (links === null) {
           return;
         }
 
         this.log(`Authenticate URL : ${links.login_url}`);
         cli.open(links.login_url);
-
         cli.action.start('Waiting');
         const login = await this.checkSecretKey(links.poll_url);
-        const secret_key = await cli.prompt('Enter your secret_key');
-        const public_key = await cli.prompt('Enter your public_key');
-        await fs.writeFile('config.json',JSON.stringify(
-          {
-            secret_key,
-            public_key
-          });
-        console.log(login);
+        this.saveInConfig(environment, login.secret_key, login.public_key);
       } catch (error) {
         this.error(error.message);
       }
+      cli.action.stop();
     }
-    cli.action.stop();
   }
 }
-
